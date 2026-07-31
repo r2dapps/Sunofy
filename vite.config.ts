3b4b95d0ee6f545e7615f34d1e3a5e64bf80c5e8
R2D Apps <rajavamsi009@gmail.com>
2026-07-31 16:54:00 +0530

    Use relative base path ./ in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Sunofy/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
